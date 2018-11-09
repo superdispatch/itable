c675df40f86d00cb08b1a03dcd9cff6df5b1e440
doniyor2109 <doniyor2109@mail.ru>
2018-11-09 12:13:58 +0500

    Added new package @itable/html for standard html table

---
+++ packages/html/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import table from '@itable/core/dist/enhancers/table';
+import tableBody from '@itable/core/dist/enhancers/tableBody';
+import tableHead from '@itable/core/dist/enhancers/tableHead';
+import tableData from '@itable/core/dist/enhancers/tableData';
+import tableHeader from '@itable/core/dist/enhancers/tableHeader';
+import tableRow from '@itable/core/dist/enhancers/tableRow';
+
+export const Table = table('table');
+export const TableRow = tableRow('tr');
+export const TableData = tableData('td');
+export const TableHead = tableHead('thead');
+export const TableHeader = tableHeader('td');
+export const TableBody = tableBody('tbody');
+
+
