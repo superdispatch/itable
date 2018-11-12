6b069824d15e7f88dc6e1fdefafd7b2ca585d069
doniyor2109 <doniyor2109@mail.ru>
2018-11-12 14:21:26 +0500

    Use th element for TableHeader

--- packages/html/index.js
+++ packages/html/index.js
@@ -10,7 +10,7 @@ export const Table = table('table');
 export const TableRow = tableRow('tr');
 export const TableData = tableData('td');
 export const TableHead = tableHead('thead');
-export const TableHeader = tableHeader('td');
+export const TableHeader = tableHeader('th');
 export const TableBody = tableBody('tbody');
 
 
